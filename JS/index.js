5aeb9eb3b4076556e1f7c88eebf1c9572d2e668f
William Friedl <carlos.friedl@gmail.com>
2022-11-23 21:09:59 -0300

    tirando linhas duplicadas

--- JS/index.js
+++ JS/index.js
@@ -2,8 +2,6 @@ import { Router } from './router.js'
 
 const router = new Router()
 router.add('/', '/pages/home.html')
-
-router.add('/the_universe', '/pages/the_universe.css')
 router.add('/the_universe', '/pages/the_universe.html')
 router.add('/exploration', '/pages/exploration.html')
 
